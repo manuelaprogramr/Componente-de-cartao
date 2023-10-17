782fedd562a8a21d088897e9e2db5a644d3bb553
manuelaprogramr <manuelagamer43@gmail.com>
2023-10-17 19:15:30 -0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,49 @@
-import logo from './logo.svg';
+import img from './assets/image-header-desktop.jpg';
 import './App.css';
+import './responsividade/mobile.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
+    <div id="container">
+      <div className='conteudo'>
+        <div className='escrita'>
+
+        <h1>Get <span>insights</span> that help your business grow.</h1>
+        <p className= 'texto'>
+          Discover the benefits of data analytics and make better decisions regarding revenue, customer
+          experience, and overall efficiency.
+        
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+        <div className='card-list'>
+      
+      <ul>
+        <li>
+          <h2>10k+</h2>
+          <p>COMPANIES</p>
+        </li>
+
+        <li>
+          <h2>314</h2>
+          <p>TEMPLATES</p>
+        </li>
+
+        <li>
+          <h2>12M+</h2>
+          <p>QUERIES</p>
+        </li>
+
+      </ul>
+
+      </div>
+        </div>
+      
+       <picture>
+        <img className='imagem' src={img} />
+       </picture>
+      
+      </div>
+      
     </div>
   );
 }
